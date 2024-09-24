94cf21a0970e4ae20f49d5ed193797ae18b39bc4
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-09-24 12:37:41 +0200

    maj format and method with filter

--- filterMovies.js
+++ filterMovies.js
@@ -1,42 +1,37 @@
 const recentMovies = [
-    {
-      title: "The Great Adventure",
-      releaseDate: "2023-07-15",
-      actors: ["Alex Johnson", "Samantha Bloom", "Michael Cane"]
-    },
-    {
-      title: "Lost in Time",
-      releaseDate: "2023-05-22",
-      actors: ["Clara Hughes", "Robert Smith", "Emily Stone"]
-    },
-    {
-      title: "Mystery of the Blue Ocean",
-      releaseDate: "2023-08-30",
-      actors: ["Henry Gold", "Natalie Port", "Bruce Wayne"]
-    },
-    {
-      title: "Cybernetic",
-      releaseDate: "2023-09-12",
-      actors: ["Aaron Stark", "Lisa Ray", "Jeremy Irons"]
-    },
-    {
-      title: "The Hidden World",
-      releaseDate: "2023-10-05",
-      actors: ["Sophia Loren", "Daniel Craig", "Emma Watson"]
-    }
-  ];
+  {
+    title: "The Great Adventure",
+    releaseDate: "2023-07-15",
+    actors: ["Alex Johnson", "Samantha Bloom", "Michael Cane"],
+  },
+  {
+    title: "Lost in Time",
+    releaseDate: "2023-05-22",
+    actors: ["Clara Hughes", "Robert Smith", "Emily Stone"],
+  },
+  {
+    title: "Mystery of the Blue Ocean",
+    releaseDate: "2023-08-30",
+    actors: ["Henry Gold", "Natalie Port", "Bruce Wayne"],
+  },
+  {
+    title: "Cybernetic",
+    releaseDate: "2023-09-12",
+    actors: ["Aaron Stark", "Lisa Ray", "Jeremy Irons"],
+  },
+  {
+    title: "The Hidden World",
+    releaseDate: "2023-10-05",
+    actors: ["Sophia Loren", "Daniel Craig", "Emma Watson"],
+  },
+];
 
-  const chooseTitle = "great" 
+const chooseTitle = "great";
 
-  function filterByTitle (recentMovies, searchedTitle) {
-    const recentMoviesFiltered = [];
+function filterByTitle(recentMovies, searchedTitle) {
+  return recentMovies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchedTitle.toLowerCase())
+  );
+}
 
-    for (let i = 0; i < recentMovies.length; i++){
-        if(recentMovies[i].title.toLowerCase().includes(searchedTitle.toLowerCase())){
-            recentMoviesFiltered.push(recentMovies[i])
-        }
-    }
-    return recentMoviesFiltered
-  }
-
-  console.log(filterByTitle(recentMovies, chooseTitle))
+console.log(filterByTitle(recentMovies, chooseTitle));
